31dc2859ad6b471bdc9b420a84d316421b3386ad
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-23 21:32:53 +0600

    different id added

---
+++ js/login.js
@@ -0,0 +1,6 @@
+document.getElementById("login-btn").addEventListener('click', function (event) {
+    event.preventDefault(); // <---------- vejal to beginners
+    console.log("Login Button Clicked");
+})
+
+// search: form submit reloading the page
