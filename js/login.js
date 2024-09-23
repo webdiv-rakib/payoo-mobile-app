1b022184dcf4db2913b93d5ff3d8052144401508
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-23 21:50:14 +0600

    input fields phone number and pin getting

--- js/login.js
+++ js/login.js
@@ -1,6 +1,15 @@
+// step1: set event handler
 document.getElementById("login-btn").addEventListener('click', function (event) {
+    // step-2: prevent default behavior (reloading)
     event.preventDefault(); // <---------- vejal to beginners
     console.log("Login Button Clicked");
+    // step-3: get the phone number
+    const phoneNumber = document.getElementById('mobile-number').value;
+    console.log(phoneNumber);
+
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(pinNumber);
+
 })
 
 // search: form submit reloading the page
