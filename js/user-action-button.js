01c8f44d243f09c587789d6667f6ecbcf0f2fb33
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-24 19:36:55 +0600

    get add amount and pin number from user and add money button working

--- js/user-action-button.js
+++ js/user-action-button.js
@@ -1,1 +1,7 @@
-console.log('Home Added');
+document.getElementById('add-money-btn').addEventListener('click', function (event) {
+    
+    event.preventDefault();
+    const addAmount = document.getElementById('add-amount').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(addAmount, pinNumber);
+})
